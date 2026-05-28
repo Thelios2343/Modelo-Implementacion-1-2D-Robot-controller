f80069d961ecd11274ccee9685e12a0dd92dd002
Thelios <jumaun47@gmail.com>
2026-05-28 14:27:52 -0500

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,35 +1,49 @@
-import ctypes
 import os
+import ctypes
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Cargar la librería C
-_lib_path = os.path.join(os.path.dirname(__file__), "robot_calc.so")
+if os.name == "nt":
+    LIB_NAME = "robot_calc.dll"
+else:
+    LIB_NAME = "robot_calc.so"
+
+_lib_path = os.path.join(BASE_DIR, LIB_NAME)
 _lib = ctypes.CDLL(_lib_path)
 
-# Indicacion a ctypes los tipos de la función
+
+# Tipos de argumentos
 _lib.calcular_nueva_pos.argtypes = [
-    ctypes.c_int, ctypes.c_int,
-    ctypes.c_char_p, ctypes.c_int,
+    ctypes.c_int,
+    ctypes.c_int,
+    ctypes.c_char_p,
+    ctypes.c_int,
     ctypes.POINTER(ctypes.c_int),
     ctypes.POINTER(ctypes.c_int),
 ]
+
+# Tipo de retorno
 _lib.calcular_nueva_pos.restype = ctypes.c_int
 
 
 class Robot:
+
     def __init__(self):
         self.x = 0
         self.y = 0
 
     def mover(self, direccion: str, pasos: int) -> tuple[bool, str]:
         """
-        Intenta mover el robot. Devuelve (éxito, mensaje).
+        Intenta mover el robot.
+        Devuelve (éxito, mensaje).
         """
+
         nueva_x = ctypes.c_int(0)
         nueva_y = ctypes.c_int(0)
 
         valido = _lib.calcular_nueva_pos(
-            self.x, self.y,
+            self.x,
+            self.y,
             direccion.lower().encode("utf-8"),
             pasos,
             ctypes.byref(nueva_x),
@@ -39,10 +53,16 @@ class Robot:
         if valido:
             self.x = nueva_x.value
             self.y = nueva_y.value
+
             return True, f"Posición: ({self.x}, {self.y})"
-        else:
-            return False, f"Movimiento inválido. Posición actual: ({self.x}, {self.y})"
-    # --- Funcion para volver a la posicion inicial ---
+
+        return False, (
+            f"Movimiento inválido. "
+            f"Posición actual: ({self.x}, {self.y})"
+        )
+
+    # Reiniciar posición
     def reset(self):
         self.x = 0
         self.y = 0
+
